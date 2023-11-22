1dfee1b1bd582e4746ffc2c1823f7fabec687f9c
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-11-23 05:00:47 +0600

    Send access token to client

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -29,6 +30,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // Send access token to client
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     // Get all menu from database
     app.get("/menu", async (req, res) => {
